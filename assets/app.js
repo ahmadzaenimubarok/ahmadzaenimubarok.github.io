284ef7568e3d1d659e331e4ac1bed208b6abe4a6
ahmad zaeni mubarok <72987106+AA43000@users.noreply.github.com>
2024-02-26 13:55:08 +0700

    add file

---
+++ assets/app.js
@@ -0,0 +1,41 @@
+// countdown
+
+var countDownDate = new Date("March 12, 2024 00:00:00").getTime();
+
+var x = setInterval(function() {
+    var now = new Date().getTime();
+
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+}, 1000);
+
+// slider
+
+let slideIndex = 0;
+
+function slideCards() {
+    const cards = document.querySelectorAll('.slide-item');
+    if (slideIndex === cards.length) {
+        slideIndex = 0;
+    }
+    cards.forEach((card, index) => {
+        card.style.transform = `translateX(-${slideIndex * 100}%)`;
+    });
+    slideIndex++;
+}
+
+setInterval(slideCards, 3000); // Mengatur interval untuk auto slide setiap 3 detik
